36f34a30053a65944ae682ff3715e862a9f6bc93
pacbac <jclaytonc99@gmail.com>
2019-07-17 20:42:52 -0700

    Add logos to companies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import girsLogo from '../resources/images/girs.png';
+import censusLogo from '../resources/images/census.png';
+import jhuaplLogo from '../resources/images/jhuapl.png';
 
-let Workplace = ({name}) => (
-    <div className="workplace">[{name}]</div>
-)
+const NAME_LOGO_MAP = {
+    girs: {
+        name: "Garrick Institute of Risk Sciences",
+        logo: girsLogo,
+        position: "Student Software Engineer",
+        period: "Since Nov 2018",
+        link: "https://risksciences.ucla.edu",
+        width: 350,
+    },
+    census: {
+        name: "US Census Bureau",
+        logo: censusLogo,
+        position: "Software Development Intern",
+        period: "Summer 2018",
+        link: "http://census.gov",
+        width: 150,
+    },
+    jhuapl: {
+        name: "Johns Hopkins University Applied Physics Laboratory",
+        logo: jhuaplLogo,
+        position: "High School ASPIRE Intern",
+        period: "Summer 2015",
+        link: "https://www.jhuapl.edu/",
+        width: 180,
+    }
+};
+
+const Workplace = ({
+    name,
+    logo,
+    width,
+    position,
+    period,
+    link,
+}) => (
+    <div className="workplace">
+        <h4>{name}</h4>
+        <p>{position} - {period}</p>
+        <a href={link}><img className="workplace-logo" src={logo} width={width} /></a>
+    </div>
+);
 
 class Home extends Component {
-    render(){
+    render() {
         return (
             <div id="home-content">
                 <div>
@@ -21,14 +62,12 @@ class Home extends Component {
                     <div className="home-work">
                         <h2>Where I've Worked</h2>
                         <div className="home-workplaces">
-                            <Workplace name="Garrick Institute of Risk Sciences"/>
-                            <Workplace name="US Census Bureau"/>
-                            <Workplace name="Johns Hopkins University Applied Physics Laboratory"/>
+                            {Object.keys(NAME_LOGO_MAP).map(workplace => <Workplace {...NAME_LOGO_MAP[workplace]} />)}
                         </div>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
