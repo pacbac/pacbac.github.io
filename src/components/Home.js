8c538c38408690db567385c6bb3fe0d260d2dad9
pacbac <jclaytonc99@gmail.com>
2019-07-18 22:39:08 -0700

    Design overhaul of the bio section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import profilePic from '../resources/images/profilepic.jpg';
+import pianoPic from '../resources/images/piano.jpg';
+import meleePic from '../resources/images/melee.jpg';
+import houzzLogo from '../resources/images/houzz.png';
 import girsLogo from '../resources/images/girs.png';
 import censusLogo from '../resources/images/census.png';
 import jhuaplLogo from '../resources/images/jhuapl.png';
 
 const NAME_LOGO_MAP = {
+    houzz: {
+        name: "Houzz",
+        logo: houzzLogo,
+        position: "Software Engineering Intern",
+        period: "Summer 2019",
+        link: "https://houzz.com",
+        technologies: ['React', 'PHP', 'SQL', 'GraphQL', 'Java'],
+        details: "At Houzz, I worked on the Marketplace Ads Team and Trade Growth team. In my time there, I worked on a project to add new legal consent components for advertisers on the houzz.com website, helped build a platform to assist vendors in pricing their products on the website, and implemented site perks to promote the Houzz Pro program.",
+        width: 150,
+    },
     girs: {
-        name: "Garrick Institute of Risk Sciences",
+        name: "UCLA Garrick Institute of Risk Sciences",
         logo: girsLogo,
         position: "Student Software Engineer",
         period: "Since Nov 2018",
         link: "https://risksciences.ucla.edu",
+        technologies: ['React + Material-UI', 'REST', 'Django'],
+        details: "At GIRS, I develop risk engineering tools used NASA JPL and Japan's Nuclear Regulation Authority. I have helped develop numerous frontend features, from automated academic report summaries of risk models to platform-wide internationalization to UI components for troubleshooting.",
         width: 350,
     },
     census: {
@@ -18,6 +47,8 @@ const NAME_LOGO_MAP = {
         position: "Software Development Intern",
         period: "Summer 2018",
         link: "http://census.gov",
+        technologies: ['HTML', 'CSS', 'jQuery', 'Java', 'SAS'],
+        details: "At the Census, I worked with a team of ~20 analysts and developers to develop and maintain an internal website used to store data and extract statistics from the Commodity Flow Survey. I also helped to clean the data used for their shipment calculation software, improving the accuracy of its results.",
         width: 150,
     },
     jhuapl: {
@@ -26,49 +57,153 @@ const NAME_LOGO_MAP = {
         position: "High School ASPIRE Intern",
         period: "Summer 2015",
         link: "https://www.jhuapl.edu/",
+        technologies: ['MATLAB', 'Java'],
+        details: "At APL, I contributed to an intern project where moving robots communicated with each other as a swarm to explore and map out their environment. I helped on creating the virtual environment as a test for the robot's algorithms, running tests on physical environments, and writing sections of the research paper.",
         width: 180,
     },
 };
 
+const styles = {
+    grid: {
+        marginBottom: 20,
+        width: '100%',
+    },
+    card: {
+        maxWidth: '30vw',
+        position: 'relative',
+        borderRadius: 0,
+    },
+    cardContent: {
+        padding: '16px 0',
+        '&:last-child': {
+            padding: 0,
+        },
+    },
+    divider: {
+        margin: '0 16px',
+    },
+    pic: {
+        width: '100%',
+    },
+    expansionPanel: {
+        boxShadow: '0 0 0',
+        position: 'static',
+        margin: '16px 0 0 0 !important',
+    },
+    expansionPanelSummary: {
+        margin: 0,
+    },
+    expansionPanelDetails: {
+        backgroundColor: '#f0f0f0',
+        display: 'block',
+    },
+};
+
 const Workplace = ({
+    classes,
+    expanded,
+    index,
+    handlePanelChange,
     name,
     logo,
     width,
     position,
     period,
-    link,
+    details,
+    technologies,
 }) => (
-    <div className="workplace">
-        <h4>{name}</h4>
-        <p>{position} - {period}</p>
-        <a href={link}><img className="workplace-logo" src={logo} width={width} /></a>
-    </div>
+    <React.Fragment>
+        <Divider className={classes.divider} />
+        <ExpansionPanel
+            expanded={expanded}
+            classes={{ root: classes.expansionPanel }}
+            onChange={() => handlePanelChange(index)}
+        >
+            <ExpansionPanelSummary 
+                classes={{ content: classes.expansionPanelSummary }}
+                expandIcon={<ExpandMoreIcon />}
+            >
+                <div className="workplace">
+                    <img className="workplace-logo" src={logo} width={width} />
+                    <Typography variant="subtitle1" component="p" style={{ marginBottom: 5 }}>{position} - {period}</Typography>
+                </div>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails className={classes.expansionPanelDetails}>
+                <Typography variant="subtitle2">{technologies.join(', ')}</Typography>
+                {details}
+            </ExpansionPanelDetails>
+        </ExpansionPanel>
+    </React.Fragment>
 );
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openPanel: -1, // no panel open
+        }
+    }
+
+    /**
+     * Restriction: we only allow one work experience to be expanded at a time (to prevent the card from being too long)
+     * If the current panel open is equal to the the one being "changed" (being clicked), it means to close the panel
+     * If the user clicked on a different panel, though, expand that one instead
+     * 
+     * @param i the workplace component's index (in the list of work experiences)
+     */
+    handlePanelChange = i => this.setState(({ openPanel }) => ({ openPanel: openPanel === i ? -1 : i }));
+
     render() {
+        const { classes } = this.props;
+        const { openPanel } = this.state;
         return (
-            <div id="home-content">
-                <div>
-                    <div className="home-bio">
-                        <h2>Hi! I'm Clayton Chu, a computer science student at UCLA 
-                        and an aspiring software engineer.</h2> 
-                        <p>My primary interests are in <strong>software engineering </strong> 
-                        and <strong>mathematical modeling</strong>, with <strong>machine learning</strong> and 
-                        other  <strong>pure and applied mathematical topics</strong> as secondary interests.</p>
-                        <p>In my free time, I love to play the piano and Super Smash Brothers Melee (and the Smash series
-                        in general) semi-competitively.</p>
-                    </div>
-                    <div className="home-work">
-                        <h2>Where I've Worked</h2>
-                        <div className="home-workplaces">
-                            {Object.keys(NAME_LOGO_MAP).map(workplace => <Workplace {...NAME_LOGO_MAP[workplace]} />)}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Grid
+                className={classes.grid}
+                container
+                justify="center"
+                spacing={2}
+            >
+                <Grid item>
+                    <Card className={classes.card}>
+                        <CardHeader
+                            title="Hi! I'm Clayton." 
+                            subheader="UCLA CS student + aspiring software engineer"
+                        />
+                        <CardMedia component="img" src={profilePic} className={classes.pic} />
+                        <CardContent>
+                            <Typography variant="h5" component="h2"></Typography>
+                            <Typography variant="body1" component="p">
+                                I am a UCLA student pursuing a Bachelors in computer science.
+                                My primary interests are in software engineering 
+                                and mathematical modeling, with machine learning and 
+                                other applied mathematical topics as secondary interests.
+                            </Typography>
+                        </CardContent>
+                    </Card>               
+                </Grid>
+                <Grid item>
+                    <Card className={classes.card}>
+                        <CardContent className={classes.cardContent}>
+                            <CardHeader
+                                title="Where I've Worked"
+                                subheader="Places I've Contributed My Skills To"
+                            />
+                            <div className="home-workplaces">
+                                {Object.keys(NAME_LOGO_MAP).map((workplace, i) => (
+                                    <Workplace 
+                                        index={i}
+                                        expanded={openPanel === i}
+                                        handlePanelChange={this.handlePanelChange}
+                                        classes={classes}
+                                        {...NAME_LOGO_MAP[workplace]}
+                                    />))}
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         );
     }
 }
 
-export default Home
+export default withStyles(styles)(Home);
